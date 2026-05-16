e5bb0615f8d49516e4470d4a05cdb2d490d90930
ahmed <ahmedninja101@gmail.com>
2026-05-16 16:11:35 -0400

    first commit

---
+++ config.local.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  genius: {
+    accessToken: "b643KhsnUdHVsgDOd4x-TH5M4Ay0d0d_UX5GpqTXNpGhJCmT-xHZ8N_MCd9Ronc8",
+  },
+  tmdb: {
+    readAccessToken: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlZDY1MmQzZDVlMDhlNmQwMWVhNTZmYzE2NjgxNzBlNiIsIm5iZiI6MTc3NzA0MTQwNS4yMjQsInN1YiI6IjY5ZWI3ZmZkYTkwYWJiZTNiYmRjNzZlNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PSYMYb8NLTvhTUc4qBtp2tLOZXXpsHTRDHuXJHXoPjc",
+  },
+  supabase: {
+    url: "https://ifhrqafymsbsgckygns.supabase.co",
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlmaHJxYWZ5bXNic2dja3lnbmxzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg4MzM2ODcsImV4cCI6MjA5NDQwOTY4N30.IZhHv-TAWoO6bqIr7unp5sucq62VCFQ6T2i1l1Zdkms",
+  },
+};
